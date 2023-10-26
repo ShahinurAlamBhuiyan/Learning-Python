feefbe3761592570a9edf1671ef358b2c31294f9
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-10-27 00:37:06 +0600

    input

---
+++ Input_Output/app.py
@@ -0,0 +1,9 @@
+name = input('What is your name? ')
+print('Hi ' + name)
+
+# Exercise --> Ask two questions: person's name & favourite color. Then, print a message like "Shahin likes Black"
+person_name = input('What is your name? ')
+fav_color = input('What is your favourite color? ')
+
+print(person_name + ' likes ' + fav_color)
+
