c5906ad3e87097fa1326f5e9d25df69699231648
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-10-27 00:53:03 +0600

    type convention

---
+++ Type_Convention/app.py
@@ -0,0 +1,13 @@
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2023 - int(birth_year)
+print(type(age))
+print(age)
+
+# Exercise --> Ask a user their weight(in pounds), convert it to kg and print on the terminal
+weight_in_pound = input('Weight in pounds: ')
+weight_in_kg = float(weight_in_pound) * 0.453592
+print(weight_in_kg)
+
+
+
