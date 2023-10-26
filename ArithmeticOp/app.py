bbe69400d2081a0ee061e88af250dd82b6e92e6d
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-10-27 02:12:21 +0600

    arithmatics

---
+++ ArithmeticOp/app.py
@@ -0,0 +1,22 @@
+print(10 + 3)  # 13
+print(10 - 3)  # 7
+print(10 * 3)  # 30
+print(10 / 3)  # 3.333
+print(10 % 3)  # 1
+print(10 // 3)  # 3 . after divide taking integer part
+print(10 ** 3)  # 10 to the power 3 --> 1000
+
+x = 10
+x += 3
+
+
+# -------OPERATOR PRECIDENCE
+# parenthesis
+# exponentiation 2**3
+# multiplication or division
+# addition or subtraction
+
+x = 10 + 3 * 2 ** 2  # what is the ans? 22
+# it's work like x = 10+(3*(2**2))
+
+x = (2 + 3) * 10 - 3  # 47
