5602146896de48c47425729a97f3c9dff5a13f38
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-10-27 00:28:23 +0600

    variables

---
+++ Variables/app.py
@@ -0,0 +1,18 @@
+price = 10  # integer variable
+print('price = ', price)
+
+rating = 4.9  # decimal variable
+print('rating = ', rating)
+
+name = 'Shahinur Alam Bhuiyan'  # string variable
+print('name = ', name)
+
+is_published = True  # boolean variable
+print('published = ', is_published)
+
+# Exercise --> We check in a patient named John Smith. He's 20 years old and is a new patient.
+patient_name = 'John Smith'
+patient_age = 20
+is_new = True
+
+
