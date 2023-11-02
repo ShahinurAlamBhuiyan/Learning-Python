3c594f21fa01bd44bf2356a0efcea5decf5c0705
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-11-02 09:48:34 +0600

    generating random values

---
+++ GRV/app.py
@@ -0,0 +1,11 @@
+# GRV --> Generating Random Values
+# some built-in module
+
+# learn more - https://docs.python.org/3/py-modindex.html
+
+import random
+for i in range(3):
+    print(random.random())  # random value 0 to 1
+    print(random.randint(10, 20))  # random value 10 to 20
+
+    # 3.40.45 yt
